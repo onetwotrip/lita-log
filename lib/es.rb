710cc9e9ffe5538dd08af584dc54e9f6e9e553fe
Antony Ryabov <anton.ryabov@onetwotrip.com>
2018-10-04 11:40:19 +0300

    refactored

--- lib/es.rb
+++ lib/es.rb
@@ -1,28 +1,14 @@
 # Singleton for accessing ElasticSearch
 class ES
   class << self
-    def timestamp
-      Time.now.utc.getlocal.iso8601
-    end
+    def put(message)
+      message[:@timestamp] = Time.now.utc.getlocal.iso8601
 
-    def type_name
-      'lita'
-    end
-
-    def index_name
-      # to get current es index name
-      'lita-' + (Time.now.strftime '%Y.%m.%d')
-    end
-
-    def put(msg, tag)
-      msg[:@timestamp] = timestamp
-      msg[:tag]        = tag
-      msg              = {
-        index: index_name,
-        type: type_name,
-        body: msg
-      }
-      es.index msg
+      es.index({
+        index: 'lita-' + (Time.now.strftime '%Y.%m.%d'),
+        type:  'lita',
+        body:  message
+      })
     end
 
     def health
